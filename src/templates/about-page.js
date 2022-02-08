c5702cb923b9d9cd00ccf7e76265135cae9cda35
scalabrindario <scalabrin.dario@gmail.com>
2022-02-08 10:49:16 +0100

    Added timeline

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -27,7 +27,62 @@ const AboutPage = ({ data }) => {
         <h1>{frontmatter.title}</h1>
         <article dangerouslySetInnerHTML={{ __html: html }} />
       </div>
-      <p class="timeline__block__text">Ciao mi chiamo Dario</p>
+        <section class="timeline">
+            <div class="container">
+                <div class="timeline__wrapper">
+                    <div class="timeline__progressbar"></div>
+                    <div class="timeline__block">
+                        <div class="timeline__block__bullet-point">
+                            <span class="timeline__block__circle"></span>
+                        </div>
+                        <div class="timeline__block__head">
+                            <h1 class="timeline__block__title"><h2>Start of Master’s degree</h2></h1>
+                        </div>
+                        <div class="timeline__block__body">
+                            <p class="timeline__block__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio nihil id animi et ipsum itaque veniam minima aliquid ad expedita.</p>
+                            <p class="timeline__block__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio nihil id animi et ipsum itaque veniam minima aliquid ad expedita.</p>
+                        </div>
+                    </div>
+                    <div class="timeline__block">
+                        <div class="timeline__block__bullet-point">
+                            <span class="timeline__block__circle"></span>
+                        </div>
+                        <div class="timeline__block__head">
+                            <h1 class="timeline__block__title">February 2020</h1>
+                        </div>
+                        <div class="timeline__block__body">
+                            <p class="timeline__block__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio nihil id animi et ipsum itaque veniam minima aliquid ad expedita.</p>
+                            <p class="timeline__block__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio nihil id animi et ipsum itaque veniam minima aliquid ad expedita.</p>
+                        </div>
+                    </div>
+                    <div class="timeline__block">
+                        <div class="timeline__block__bullet-point">
+                            <span class="timeline__block__circle"></span>
+                        </div>
+                        <div class="timeline__block__head">
+                            <h1 class="timeline__block__title">March 2020</h1>
+                        </div>
+                        <div class="timeline__block__body">
+                            <p class="timeline__block__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio nihil id animi et ipsum itaque veniam minima aliquid ad expedita.</p>
+                            <p class="timeline__block__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio nihil id animi et ipsum itaque veniam minima aliquid ad expedita.</p>
+                        </div>
+                    </div>
+                    <div class="timeline__block">
+                        <div class="timeline__block__bullet-point">
+                            <span class="timeline__block__circle"></span>
+                        </div>
+                        <div class="timeline__block__head">
+                            <h1 class="timeline__block__title">April 2020</h1>
+                        </div>
+                        <div class="timeline__block__body">
+                            <p class="timeline__block__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio nihil id animi et ipsum itaque veniam minima aliquid ad expedita.</p>
+                            <p class="timeline__block__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio nihil id animi et ipsum itaque veniam minima aliquid ad expedita.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     </Layout>
   )
 }
